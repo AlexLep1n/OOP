9b892e27ba6e48ca3d8be3c8fbce2b38257628dc
Alex Lepin <alexlepin@mail.ru>
2022-12-13 18:22:13 +0300

    lesson 1 done
    Some fix

--- lesson_1/src/main/java/com/oop_hw/App.java
+++ lesson_1/src/main/java/com/oop_hw/App.java
@@ -1,5 +1,8 @@
 package com.oop_hw;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // 1. Создайте унаследованный класс ГорячийНапиток с дополнительным полем int температура.
 
 // 2. Создайте класс АвтоматГорячихНапитков, реализующий интерфейс ТорговыйАвтомат и реализуйте перегруженный метод getProduct(int name, int volume, int temperature), выдающий продукт, соответствующий имени, объему и температуре.
@@ -8,6 +11,19 @@ package com.oop_hw;
 
 public class App {
     public static void main(String[] args) {
+        HotDrink hotDrink1 = new HotDrink("Coffe", 1, 50, 1, 90, 0.5);
+        HotDrink hotDrink2 = new HotDrink("Latte", 1, 65, 2, 90, 0.45);
+        HotDrink hotDrink3 = new HotDrink("Capuchino", 1, 40, 3, 90, 0.4);
 
+        List<HotDrink> hotDrinks = new ArrayList<>();
+        hotDrinks.add(hotDrink1);
+        hotDrinks.add(hotDrink2);
+        hotDrinks.add(hotDrink3);
+
+        HotDrinksVMachine hotDrinksVMachine = new HotDrinksVMachine(hotDrinks);
+        System.out.println(hotDrinksVMachine); // все имеющиеся напитки в автомате горячих напитков
+
+        // покупатель получает выбранный напиток
+        System.out.println("Ваш напиток: " + hotDrinksVMachine.getProduct(1, 0.5, 90));
     }
 }
